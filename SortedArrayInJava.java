c80f44a8f97befeccb3966d270c1fa8c4cc29a79
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2024-03-28 22:36:34 +0530

    SortedArrayInJava.java

--- SortedArrayInJava.java
+++ SortedArrayInJava.java
@@ -8,16 +8,17 @@ public class SortedArrayInJava {
         for(int i=0;i<n;i++) {
             arr[i]=sc.nextInt();
         }
-          boolean isAscending=true;
+        //Is ascending or not 
+          boolean isAscending = true;
         for(int i=0;i<n-1;i++) {
             if(arr[i] > arr[i+1]) {
                 isAscending=false;
             }
         }
         if(isAscending) {
-            System.out.println("The array is sorted in ascending order");
+            System.out.println("Array is sorted in ascending order");
         } else {
-            System.out.println("The array is not sorted in ascending order");
+            System.out.println("Array is not sorted in ascending order");
         }
 
     }
